eabdec5607de7492f84e0ebb868fcaa1004ee795
one30 <837603674@qq.com>
2019-10-16 12:37:37 +0800

    v1.0

--- app/src/main/java/com/example/firstscreendemo/MainActivity.java
+++ app/src/main/java/com/example/firstscreendemo/MainActivity.java
@@ -7,6 +7,7 @@ import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.util.TypedValue;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -19,31 +20,17 @@ import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
-//    private boolean clicked = false;
-//    private Button myButton;
-//    private EditText myEditText;
     private ImageView myImageView;
-
+    private int flag;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
 
-
-        //定义三个子控件
-//        myButton = new Button(this);
-//        myButton.setText("Press me");
-//
-//        myEditText = new EditText(this);
-//        myEditText.setHint("See me");
-
+        //定义一个子控件
         myImageView = new ImageView(this);
-
-
-        //为控件设置ID
-//        myButton.setId(1);
-//        myEditText.setId(2);
-          myImageView.setId(3);
+        //为子控件设置ID
+        myImageView.setId(1);
+        flag=2;
 
         AssetManager assets = getAssets();
         InputStream is = null;
@@ -55,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
         }
         BitmapFactory.Options options = new BitmapFactory.Options();
         Bitmap bitmap = BitmapFactory.decodeStream(is,null,options);
-        //myImageView = (ImageView)findViewById(R.id.3);
+        //myImageView = (ImageView)findViewById(R.id.1);
         myImageView.setImageBitmap(bitmap);
 
         //定义父容器和设置属性
@@ -63,40 +50,44 @@ public class MainActivity extends AppCompatActivity {
         //设置背景颜色
         myLayout.setBackgroundColor(Color.BLUE);
 
-        //设置Button布局参数
-//        RelativeLayout.LayoutParams buttonParams =
-//                new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT,
-//                        RelativeLayout.LayoutParams.WRAP_CONTENT);
-//        buttonParams.addRule(RelativeLayout.CENTER_HORIZONTAL);
-//        buttonParams.addRule(RelativeLayout.CENTER_VERTICAL);
+        if(flag==1)//居中显示
+        {
+            //设置ImageView的布局参数
+            RelativeLayout.LayoutParams imageParams =
+                    new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT,
+                            RelativeLayout.LayoutParams.WRAP_CONTENT);
+            imageParams.addRule(RelativeLayout.CENTER_VERTICAL);
+            imageParams.addRule(RelativeLayout.CENTER_HORIZONTAL);
+            //将布局添加到父容器中
+            myLayout.addView(myImageView,imageParams);
+        }
+        else if(flag==2)//铺满
+        {
+            DisplayMetrics metric = new DisplayMetrics();
+            getWindowManager().getDefaultDisplay().getRealMetrics(metric);
+//            int width = metric.widthPixels; // 宽度（PX）
+//            int height = metric.heightPixels; // 高度（PX）
 
-        //设置EditText的布局参数
-//        RelativeLayout.LayoutParams textParams =
-//                new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT,
-//                        RelativeLayout.LayoutParams.WRAP_CONTENT);
-//        textParams.addRule(RelativeLayout.ABOVE,myButton.getId());
-//        textParams.addRule(RelativeLayout.CENTER_HORIZONTAL);
-//        textParams.setMargins(0,0,0,80);
-
-        //设置EditText的宽度为指定大小宽度，要相应的dp转化为px
-//        Resources r = getResources();
-//        int px = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,200,r.getDisplayMetrics());
-
-        //设置ImageView的布局参数
-        RelativeLayout.LayoutParams imageParams =
-                new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT,
-                        RelativeLayout.LayoutParams.WRAP_CONTENT);
-        //imageParams.addRule(RelativeLayout.BELOW,myButton.getId());
-        imageParams.addRule(RelativeLayout.CENTER_VERTICAL);
-        imageParams.addRule(RelativeLayout.CENTER_HORIZONTAL);
+            //设置ImageView的布局参数
+            RelativeLayout.LayoutParams imageParams =
+                    new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT,
+                            RelativeLayout.LayoutParams.MATCH_PARENT);
+//            RelativeLayout.LayoutParams imageParams =
+//                    new RelativeLayout.LayoutParams(width,height);
+            imageParams.addRule(RelativeLayout.CENTER_VERTICAL);
+            imageParams.addRule(RelativeLayout.CENTER_HORIZONTAL);
+//            imageParams.width=width;
+//            imageParams.height=height;
+            //拉伸平铺图片
+            myImageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            //将布局添加到父容器中
+            myLayout.addView(myImageView,imageParams);
+        }
 
 
-        //myEditText.setWidth(px);
-        //将布局添加到父容器中
-//        myLayout.addView(myButton,buttonParams);
-//        myLayout.addView(myEditText,textParams);
-        myLayout.addView(myImageView,imageParams);
 
         setContentView(myLayout);
     }
+
+
 }
